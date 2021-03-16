418bc901c41708b8f69bab30ece5abcbaea19b56
Ivan Shchankin <ivantreada@gmail.com>
2021-03-17 00:03:57 +0300

    step 1

---
+++ content/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class BlockType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Content(models.Model):
+    block = models.ForeignKey(BlockType, on_delete=models.CASCADE)
+
+
+class Block(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    img = models.ImageField(upload_to='media/content')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
